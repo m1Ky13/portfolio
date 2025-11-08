16b7f7b1ff23d39f7d58e54825bf79b370cad8b3
m1Ky13 <michaelgamerpc@gmail.com>
2025-11-07 22:06:54 -0500

    new portfolio project1

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// JavaScript file
+console.log('Portfolio page loaded!');
+
+// Example: Add an event listener when the page loads
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM is ready!');
+});
+
